481f4596b9d6797200c215c50a9a97a5c50384ed
Gisy7 <gisyestevezfp@gmail.com>
2026-04-11 17:36:52 +0100

    Cambios en supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,8 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
+import { enviroment } from '../config/enviroment';
 
-const supabaseUrl = 'https://xruckfgtrvrouqyyecco.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhydWNrZmd0cnZyb3VxeXllY2NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1ODc1NDMsImV4cCI6MjA5MTE2MzU0M30.5HgAKiT6FVEjmp6h5cFTDjGNaCH0vycoH-OMzAAKGBs';
+const supabaseUrl = enviroment.SUPABASE_URL;
+const supabaseAnonKey = enviroment.SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
